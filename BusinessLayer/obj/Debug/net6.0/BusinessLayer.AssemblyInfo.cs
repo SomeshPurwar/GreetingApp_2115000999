86b3eb3375934bfd365dcba2afee41c9ad545f6a
SomeshPurwar <somesh.purwar20@gmail.com>
2025-03-04 01:10:58 +0530

    [Somesh] add:Adding UC2.

--- BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7edc25a70d146e242cc461275fe18dca6c67205")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2a47509e70c387faf2d4d0fb68928c9b0d38842")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HelloGreetingApplication/obj/Debug/net6.0/HelloGreetingApplication.AssemblyInfo.cs
+++ HelloGreetingApplication/obj/Debug/net6.0/HelloGreetingApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloGreetingApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7edc25a70d146e242cc461275fe18dca6c67205")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2a47509e70c387faf2d4d0fb68928c9b0d38842")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloGreetingApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloGreetingApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
